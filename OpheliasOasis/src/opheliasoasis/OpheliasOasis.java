78d53a9555eebc61a9093b992ddcaed5086ea4ba
Computer <Computer@172.31.205.159>
2019-04-05 19:00:16 -0400

    Trial

--- OpheliasOasis/src/opheliasoasis/OpheliasOasis.java
+++ OpheliasOasis/src/opheliasoasis/OpheliasOasis.java
@@ -15,7 +15,7 @@ public class OpheliasOasis {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Software Engineering");
     }
     
 }
