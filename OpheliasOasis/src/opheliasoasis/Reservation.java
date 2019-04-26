9e1b6a51098d61a8dc1d936ae9a2ca70f0f410c8
Computer <Computer@172.31.228.7>
2019-04-26 19:45:56 -0400

    check_in function, default constructor, flag for check in added

--- OpheliasOasis/src/opheliasoasis/Reservation.java
+++ OpheliasOasis/src/opheliasoasis/Reservation.java
@@ -5,7 +5,9 @@
  */
 package opheliasoasis;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.List;
+import javafx.util.Pair;
 
 /**
  * Represents a single reservation and valid operations on it.
@@ -13,24 +15,109 @@ import java.util.Date;
  * @author roanm
  */
 public class Reservation {
+    private ResType res_type;
+    private LocalDate date_in;
+    private LocalDate date_out;
+    private String name;
+    private String email;
+    private CreditCard cc;
+    private int roomNumber;
+    private Boolean is_Changed;
+    private Boolean is_Cancled;
+    private Boolean is_CheckedOut;
+    private Boolean is_CheckedIn;
+
 
     public enum ResType {
         prepaid, sixty_day, conventional, incentive
     };
+    public Reservation(){}
 
-    public Reservation(ResType res_type, Date date_in, Date date_out,
+    public Reservation(ResType res_type, LocalDate date_in, LocalDate date_out,
                        String name, CreditCard cc, String email) {
-    }
+        this.res_type = res_type;
+        this.date_in = date_in;
+        this.date_out = date_out;
+        this.name = name;
+        this.cc = cc;
+        this.email = email;
+        roomNumber = 00;
+        is_Changed = false;
+        is_Cancled = false;
+        is_CheckedOut = false;
+        is_CheckedIn = false;
 
-    public void change_date(Date date_in, Date date_out) {
     }
+    
 
+    public void change_date(LocalDate date_in, LocalDate date_out) {
+
+    }
     public void cancel() {
     }
 
-    public void check_in(int room_num) {
+    public void check_in(int res_id) {
+        
+       System.out.println("The person has checked in and the reservation status is changed from expected arrival to occupied(checked in)");
     }
 
-//    public get( ) {} // Placeholder for all getters
+    
+    public ResType getResType(){
+        return res_type;
+    }
+    public LocalDate getDateIn(){
+        return date_in;
+    }
+    public LocalDate getDateOut(){
+        return date_out;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public CreditCard getCreditCard(){
+        return cc;
+    }
+    public int getRoomNumber(){
+        return roomNumber;
+    }
+    public Boolean getChangedStatus(){
+        return is_Changed;
+    }
+    public Boolean getCancledStatus(){
+        return is_Cancled;
+    }
+    public Boolean getCheckedOutStatus(){
+        return is_CheckedOut;
+    }
+    public void setDateIn(LocalDate NewDate_in){
+        date_in= NewDate_in;
+    }
+    public void setDateOut(LocalDate NewDate_out){
+        date_out = NewDate_out;
+    }
+    public void setName(String NewName){
+        name = NewName;
+    }
+    public void setEmail(String NewEmail){
+        email = NewEmail;
+    }
+    public void setCreditCard(CreditCard NewCC){
+        cc = NewCC;
+    }
+    public void setRoomNumber(int NewRoomNumber){
+        roomNumber = NewRoomNumber;
+    }
+    public void setChangedStatus(Boolean NewStatus){
+        is_Changed = NewStatus;
+    }
+    public void setCancledStatus(Boolean NewStatus){
+        is_Cancled = NewStatus;
+    }
+    public void setCheckOutStatus (Boolean NewStatus){
+        is_CheckedOut = NewStatus;
+    }
 
 }
